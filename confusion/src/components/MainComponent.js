12e3d5bc17642635f3fda7a8c635a5e4a4d8fb63
eshaansinghparihar <eshaan.singh.parihar@gmail.com>
2020-06-01 10:09:38 +0530

    Assignment Week 2 Completed

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent1';
 import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponents';
 import { DISHES } from '../shared/dishes';
@@ -43,6 +44,7 @@ class Main extends Component {
             comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
       );
     };
+    
     return (
       <div>
         <Header />
@@ -50,8 +52,8 @@ class Main extends Component {
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={Contact} />} />
-              
+              <Route exact path='/contactus' component={Contact} />
+              <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
               <Redirect to="/home" />
           </Switch>
         <Footer />
