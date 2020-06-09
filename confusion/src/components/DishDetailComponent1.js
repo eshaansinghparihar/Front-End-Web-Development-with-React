b371c636cf9e69d88e6f665afe0db8e26b7d8d23
eshaansinghparihar <eshaan.singh.parihar@gmail.com>
2020-06-09 16:44:00 +0530

    Fetch POST Comments

--- confusion/src/components/DishDetailComponent1.js
+++ confusion/src/components/DishDetailComponent1.js
@@ -42,12 +42,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(
-      this.props.dishId,
-      values.rating,
-      values.author,
-      values.comment
-    );
+    this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
   }
 
   render() {
@@ -146,8 +141,7 @@ function RenderDish({ dish}) {
   );
 }
 
-function RenderComments({ comments, addComment, dishId }) {
-  
+function RenderComments({comments, postComment, dishId}) {  
     return (
       <div className="col-12 col-md-5 m-1">
         <h4>Comments</h4>
@@ -166,7 +160,7 @@ function RenderComments({ comments, addComment, dishId }) {
                 </li>
             );
           })}
-        <CommentForm dishId={dishId} addComment={addComment} />
+        <CommentForm dishId={dishId} postComment={postComment} />
       </div>
     );
   
@@ -210,7 +204,7 @@ else if (props.dish != null)
           <RenderDish dish={props.dish}/>
           <RenderComments
             comments={props.comments}
-            addComment={props.addComment}
+            postComment={props.postComment}
             dishId={props.dish.id}
           />
         </div>
